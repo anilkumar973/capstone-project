8d1771d5bf85cf43368e5418c459eb10fe0631cb
anilkumar973 <anilkumarhadapad143@gmail.com>
2026-01-02 14:27:05 +0530

    Initial commit: CI/CD capstone project setup

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
+
+@app.route("/health")
+def health():
+    return {"status": "ok", "env": os.getenv("APP_ENV")}
+
+@app.route("/users")
+def users():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
+    cur.execute("INSERT INTO users (name) VALUES ('Anil');")
+    conn.commit()
+    cur.execute("SELECT * FROM users;")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
